2158a80e4e47453a2c0dcf87360ea280c3dd5002
Mikhail Safonov <ms33@inbox.ru>
2019-09-11 19:43:34 +0300

    commit 2019-09-11_01

---
+++ 000577_06_Math_Unfamiliar_Expressions.py
@@ -0,0 +1,27 @@
+# http://www.asmarterwaytolearn.com/python/index-of-exercises.html
+
+# modulo
+whats_left_over = 10 % 3
+print("10 % 3 =", whats_left_over)
+
+age = 12
+age += 50
+print("12 += 50:", age)
+
+age = 12
+age *= 3
+print("12 *= 3:", age)
+
+# Don't forget that you can always use a variable instead of a number
+age = 12
+amount_to_increment = 3
+age += amount_to_increment
+print("12 += 3:", age)
+
+var1 = 3
+var1 *= 2
+print("3 *= 2:", var1)
+
+
+
+
