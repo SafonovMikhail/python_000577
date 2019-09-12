663bea63595b8df25429bedcb01fed7a62b3c027
Mikhail Safonov <ms33@inbox.ru>
2019-09-12 07:25:39 +0300

    commit 2019-09-12_01

---
+++ 000577_07_Math_ElimAmbiguity.py
@@ -0,0 +1,30 @@
+# http://www.asmarterwaytolearn.com/python/index-of-exercises.html
+
+total_cost = 1 + 3 * 4
+print(total_cost)
+
+total_cost = 1 + (3 * 4)
+print(total_cost)
+
+result_of_computation = (2 * 4) * 4 + 2
+print(result_of_computation)
+
+'''
+Exercise 1
+If you haven't bothered to memorize precedents rules, there are two ways you 
+might interpret the math below. If you interpret it one way, total_cost is 16. 
+If you interpret it the other way, total_cost is _____.
+'''
+total_cost = 1 + 3 * 4
+print('total_cost = 1 + 3 * 4 =', total_cost)
+
+'''
+Exercise 2
+What is the value of total_cost?
+total_cost = 1 + (3 * 4)
+'''
+print('Exercise 2:', total_cost)
+
+'''
+
+'''
