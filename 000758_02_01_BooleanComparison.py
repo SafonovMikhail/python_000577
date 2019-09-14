1709611f6992b9c86483ecc90b4e83278beb2552
Mikhail Safonov <ms33@inbox.ru>
2019-09-14 12:03:08 +0300

    commit 2019-09-14_12.00

--- 000758_02_01_BooleanComparison.py
+++ 000758_02_01_BooleanComparison.py
@@ -7,12 +7,10 @@ for instance by using the equal operator ==.
 Be careful not to confuse assignment (one equals sign)
 with comparison (two equals signs).
 '''
-
+print("000758_02_01_BooleanComparison:01")
 my_boolean = True
 print(my_boolean)
-
 print(2 == 3)
-
 print("hello" == "hello")
 
 '''
@@ -29,3 +27,7 @@ Another comparison operator, the not equal operator (!=),
 evaluates to True if the items being compared aren't equal, 
 and False if they are.
 '''
+print("000758_02_01_BooleanComparison:02")
+print(1 != 1)
+print("eleven" != "seven")
+print(2 != 0)
