57adf2d711e66615953b756b0451352dff3688ce
Mikhail Safonov <ms33@inbox.ru>
2019-09-15 14:05:18 +0300

    000560_02_2019-09-15_14.01

--- 000560_02_05_OperatorsRpecedence.py
+++ 000560_02_05_OperatorsRpecedence.py
@@ -1,17 +1,17 @@
-# 000560_02_05_ex01_priority.py
+print("000560_02_05_ex01_priority")
 print(False == False or True)
 print(False == (False or True))
 print((False == False) or True)
-# 000560_02_05_ex01_priority.py
+print("000560_02_05_ex01_priority")
 if 1 + 1 * 3 == 6:
     print('yes')
 else:
     print('no')
 
-# 000560_02_05_task02_priority.py
+print("000560_02_05_task02_priority")
 x = 4
 y = 2
 if not 1 + 1 == y or x == 4 and 7 == 8:
     print('yes')
-elif (x > y):
+elif x > y:
     print('no')
