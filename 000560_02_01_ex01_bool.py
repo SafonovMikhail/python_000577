ed2d02740b392672af9d6bb9673239e60aec8dd2
Mikhail Safonov <ms33@inbox.ru>
2019-09-15 13:42:15 +0300

    000560_01_2019-09-15_13.40

--- 000560_02_01_ex01_bool.py
+++ 000560_02_01_ex01_bool.py
@@ -1,6 +1,21 @@
-#000560_02_01_ex01_bool.py
+#000560_02_01_ex01_bool
 
 myBoolean = True
 print (myBoolean)
 print (2==3)
 print ("hello"=="hello!")
+
+# 000560_02_01_ex02_comparison
+print("Операции сравнения 01")
+print()
+print(1 != 1)
+print("eleven" != "seven")
+print(2 != 10)
+print()
+print("Операции сравнения 02")
+print(7 > 5)
+print(10 < 10)
+print()
+print("Операции сравнения 03")
+print(7 <= 8)
+print(9 >= 9.0)
--- 000560_02_01_ex01_bool_ERR.py
+++
@@ -1,5 +0,0 @@
-#000560_02_01_ex01_bool.py
-myBoolean = True
-print (myBoolean)
-2==3
-"hello"=="hello"
