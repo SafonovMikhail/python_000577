3f06a481510562e56b877daa65bd61395315e8c6
Mikhail Safonov <ms33@inbox.ru>
2019-09-15 17:23:53 +0300

    000560_02_07_Lists_2019-09-15_17.21_PEP8

--- 000560/000560_02_07_Lists.py
+++ 000560/000560_02_07_Lists.py
@@ -1,49 +1,48 @@
-#000560_02_07_Lists.py
+# 000560_02_07_Lists.py
 
-words = ['Hello','world','!']
-print (words[0])
-print (words[1])
-print (words[2])
+words = ['Hello', 'world', '!']
+print(words[0])
+print(words[1])
+print(words[2])
 
-#000560_02_07_task01_lists.py
-#Каким будет результат вывода
-nums = [5,4,3,2,1]
-print (nums[1])
+# 000560_02_07_task01_lists.py
+# Каким будет результат вывода
+nums = [5, 4, 3, 2, 1]
+print(nums[1])
 
-#000560_02_07_ex02_lists.py
-#Для создания списка используем квадратные скобки (brackets)
+# 000560_02_07_ex02_lists.py
+# Для создания списка используем квадратные скобки (brackets)
 empty_list = []
-print (empty_list)
+print(empty_list)
 
-#000560_02_07_ex02_lists.py
-#Для создания списка используем квадратные скобки (brackets)
+# 000560_02_07_ex02_lists.py
+# Для создания списка используем квадратные скобки (brackets)
 empty_list = []
-print (empty_list)
+print(empty_list)
 
-#000560_02_07_ex03_lists.py
+# 000560_02_07_ex03_lists.py
 number = 3
-things = ['string',0,[1,2,number],4.56]
-print (things[1])
-print (things[2])
-print (things[2][2])
+things = ['string', 0, [1, 2, number], 4.56]
+print(things[1])
+print(things[2])
+print(things[2][2])
 
-#000560_02_07_task03_lists.py
-#Создайте список и выведите на экран его третий элемент
-things = [1,2,3]
-print (things[2])
-#обратить внимание, что третий элемент имеет индекс 2(!)
+# 000560_02_07_task03_lists.py
+# Создайте список и выведите на экран его третий элемент
+things = [1, 2, 3]
+print(things[2])
+# обратить внимание, что третий элемент имеет индекс 2(!)
 
-#000560_02_07_ex04_lists.py
-#Ошибка при вызове несуществующего элемента
-#Индексирование строк
+# 000560_02_07_ex04_lists.py
+# Ошибка при вызове несуществующего элемента
+# Индексирование строк
 str = 'Hello world!'
-print (str[6])
-#>>> w
+print(str[6])
+# >>> w
 
-#000560_02_07_task04_lists_err.py
-#Какая строка кода приведет к ошибке?
-num = [5,4,3,[2],1]
-print (num[0])
-print (num[3][0])
+# 000560_02_07_task04_lists_err.py
+# Какая строка кода приведет к ошибке?
+num = [5, 4, 3, [2], 1]
+print(num[0])
+print(num[3][0])
 # print (num[5])
-
