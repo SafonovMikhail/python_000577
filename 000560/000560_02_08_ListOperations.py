240fd8d64a340b1c3340eff1024415f246797612
Mikhail Safonov <ms33@inbox.ru>
2019-09-16 18:14:02 +0300

    000560_02_08_ListsOperations_2019-09-16_18.12

--- 000560/000560_02_08_ListOperations.py
+++ 000560/000560_02_08_ListOperations.py
@@ -8,8 +8,8 @@ nums = [7, 7, 7, 7, 7]
 nums[2] = 5
 print(nums)
 
-print("")
-print("000560_02_08_task01_listOp")
+# print("")
+print("\n000560_02_08_task01_listOp")
 nums = [5, 4, 3, [2], 1]
 print(nums)
 nums[3] = nums[1]
@@ -22,12 +22,10 @@ print(nums)
 nums[3] = nums[1]
 print('nums[3] = nums[1]')
 print(nums)
-print("nums[3]:",nums[3])
+print("nums[3]:", nums[3])
 
-
-
-print("")
-print("000560_02_08_ex02_ListOperations:AddMultiply")
+# print("")
+print("\n000560_02_08_ex02_ListOperations:AddMultiply")
 nums = [5, 4, 3, [2], 1]
 print('nums')
 print(nums)
@@ -44,18 +42,15 @@ print(nums * 3)
 # рассмотреть различные варианты умножения элементов списка:
 # поэлементно, каждый элемент по 3 раза
 
-
-
-print("")
-print("000560_02_08_task02_list")
+# print("")
+print("\n000560_02_08_task02_list")
 # переназначьте 2-й элемент и выведите список на экран
 nums = [5, 4, 3, [2], 1]
 print('nums[1]=22')
 nums[1] = 22
 print(nums)
 
-print("")
-print("000560_02_08_ex03_listBool")
+print("\n000560_02_08_ex03_ListOp")
 # Проверка на наличие элемента в списке
 words = ['spam', 'egg', 'spam', 'sausage']
 print("words[]")
@@ -67,8 +62,46 @@ print('egg' in words)
 print("'tomato' in words")
 print('tomato' in words)
 
-print("")
-print("000560_02_08_task03_listIfElse")
+print('\n000560_02_08_ex03-01_ListOp:"hello" in "hello world"')
+print("hello" in "hello world")
+'''
+because every string is treated as a list.
+and "hello" is a substring of "hello world"
+'''
+
+print('\n000560_02_08_ex03-02_ListOp:"elements in lists"')
+x = ["eggs", " spam"]
+print("pam" in x)
+# False
+print("pam" in x[1])
+# True
+
+print('\n000560_02_08_ex03-03_ListOp:\"example \'elements in list\'"')
+A = 3
+print("A:", A)
+B = "s"
+print("B:", B)
+C = [A, B, 1, "Hello World"]
+print("C:", C)
+print("1 in C:", 1 in C)  # True
+print("A in C:", A in C)  # True
+print('"s" in C:', "s" in C)  # True
+print("B in C:", B in C)  # True
+print('3 in C:', 3 in C)  # True
+print('"H" in C:', "H" in C)  # False
+print('"Hello" in C:', "Hello" in C)  # False
+print('"Hello World" in C', "Hello World" in C)  # True
+
+# print()
+print('\n000560_02_08_ex03-03_ListOp:"example \'elements in list\' 02"')
+tools = ["wrench", "putty knife", "jigsaw", ["nut", "bolt", "washer"], "hacksaw"]
+print("washer" in tools[3])
+print("washer" in tools)
+print(tools[2][3:5] in tools[4])
+print(tools[2][3:5] in tools)
+
+# print("")
+print("\n000560_02_08_task03_listIfElse")
 # Каким будет результат программы?
 nums = [10, 9, 8, 7, 6, 5]
 nums[0] = nums[1] - 5
@@ -77,8 +110,8 @@ if 4 in nums:
 else:
     print(nums[4])
 
-print("")
-print("000560_02_08_ex04_listNot")
+# print("")
+print("\n000560_02_08_ex04_listNot")
 # Проверка отсутствия элемента в списке
 nums = [10, 9, 8, 7, 6, 5]
 print(not 4 in nums)
@@ -86,8 +119,8 @@ print(4 not in nums)
 print(not 9 in nums)
 print(9 not in nums)
 
-print("")
-print("000560_02_08_task04_listYest")
+# print("")
+print("\n000560_02_08_task04_listYest")
 # Проверка наличия элемента в списке
 letters = ['a', 'b', 'z', ]
 if 'z' in letters:
