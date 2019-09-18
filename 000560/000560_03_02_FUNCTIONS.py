beebb03a9a28b8cbafdb41fc914e59a95a7a44ff
Mikhail Safonov <ms33@inbox.ru>
2019-09-18 17:25:20 +0300

    000560_03_FUNCTIONS_17.23

---
+++ 000560/000560_03_02_FUNCTIONS.py
@@ -0,0 +1,49 @@
+# 000560_03_02_FUNCTIONS.py
+
+print()
+print("000560_03_02_ex01_functions_example")
+
+
+# Функция, три раза выводящая "spam"
+def my_function():
+    print('spam')
+    print('spam')
+    print('spam')
+
+
+my_function()
+
+print()
+print("000560_03_02_task01_functions_hello")
+
+
+# заполнить пропуски, с тем, чтобы функция выводила "hello!"
+def hello():
+    print('hello')
+
+
+hello()
+# input()
+
+print()
+print("000560_03_02_ex02_functions_def?")
+
+
+# сначала определение функции, затем - вызов, иначе - ошибка
+# hello02()
+def hello02():
+    print('hello02')
+
+
+hello02()
+# input()
+
+print()
+print("000560_03_02_task02_functions_rearrange")
+# rearrange code
+
+def sayHi():
+    print("Hi!")
+
+
+sayHi()
