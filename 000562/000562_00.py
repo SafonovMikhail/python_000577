786fd2373ac9590c160bb2d4ec43e17dc24ace42
Mikhail Safonov <ms33@inbox.ru>
2019-10-06 14:10:04 +0300

    000562_00_2019-10-16_14.09

--- 000562/000562_00.py
+++ 000562/000562_00.py
@@ -6,18 +6,18 @@ import os
 odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57,
         59]
 right_this_minute = datetime.today().minute
-print("\n",right_this_minute)
+print("\nright_this_minute: ",right_this_minute)
 if right_this_minute in odds:
-    print("\nodd.")
+    print("\nodd minute")
 else:
-    print("\nnot an odd minute")
+    print("\neven minute")
 
 where_am_I = getcwd()
 print("\n", where_am_I)
 
-print("\nsystem: ", sys.platform)
-print("\nPythonVersion: ", sys.version)
-print("\nEnvironment: ", os.environ)
+print("\nsystem(sys.platform): ", sys.platform)
+print("\nPythonVersion(sys.version): ", sys.version)
+print("\nEnvironment(os.environ): ", os.environ)
 
 
 
