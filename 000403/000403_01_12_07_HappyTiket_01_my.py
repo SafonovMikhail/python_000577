ff191ff4bbb67a966e9c966601957283357e7c59
Mikhail Safonov <ms33@inbox.ru>
2019-11-11 05:56:39 +0300

    000403_00_2019-11-11_06.00

---
+++ 000403/000403_01_12_07_HappyTiket_01_my.py
@@ -0,0 +1,7 @@
+a = input()
+sumLeft = int(a[0])+int(a[1])+int(a[2])
+sumRright = int(a[3])+int(a[4])+int(a[5])
+if sumLeft == sumRright:
+    print("Счастливый")
+else:
+    print("Обычный")
---
+++ 000403/000403_01_12_07_HappyTiket_02_StepikSolvers.py
@@ -0,0 +1,7 @@
+a, b, c, d, e, f = input()
+l = int(a)+int(b)+int(c)
+r = int(d)+int(e)+int(f)
+if l == r:
+    "Cчастливый"
+else:
+    "Обычный"
