de08410c74105400ba4981b5cfa2f089ce245e74
Mikhail Safonov <ms33@inbox.ru>
2020-01-05 10:15:04 +0300

    000403_00_2020-01-05_10.15

---
+++ 000403/000403_02_02_Task_01_BreakContinue_other_01.py
@@ -0,0 +1,7 @@
+while True:
+    number = int(input())
+    if number >100:
+        break
+    if number <10:
+        continue
+    print(number)
---
+++ 000403/000403_02_02_Task_01_BreakContinue_other_03.py
@@ -0,0 +1,4 @@
+x = 0
+while x <= 100:
+    if x >= 10: print(x)
+    x = int(input())
