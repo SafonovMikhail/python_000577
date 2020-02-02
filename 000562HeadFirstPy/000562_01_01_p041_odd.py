1519227bb98a872c0701e32424017b784c13a873
Mikhail Safonov <ms33@inbox.ru>
2020-02-02 07:16:48 +0300

    2020-02-01_000562_HeadFirstPyResumed

---
+++ 000562HeadFirstPy/000562_01_01_p041_odd.py
@@ -0,0 +1,39 @@
+from datetime import datetime #модуль [datetima], подмодуль [datetime]
+# импортирует функциональность(?) из стандартной библиотеки
+from os import getcwd
+import sys
+import os
+
+# import datetime
+
+odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57,
+        59]
+right_this_minute = datetime.today().minute
+print("\nright_this_minute: ",right_this_minute)
+if right_this_minute in odds:
+    print("\nodd minute")
+else:
+    print("\neven minute")
+
+where_am_I = getcwd()
+print("\ngetcwd() - возвращает имя текущего каталога:" )
+# print("\n", where_am_I)
+print(where_am_I)
+
+print("\nsys: модуль для...")
+print("\nsystem(sys.platform): ", sys.platform)
+print("\nPythonVersion(sys.version): ", sys.version)
+
+print (os.getcwd())
+
+print("\nEnvironment(os.environ): ", os.environ)
+print("доступ ко всем переменным окружения")
+
+print("\nEnvironment os.getenv('PATH'): ", os.getenv('PATH'))
+print("\nEnvironment os.getenv('APPDATA'): ", os.getenv('APPDATA'))
+
+
+
+
+
+
---
+++ 000562HeadFirstPy/000562_01_01_p048_datetime.py
@@ -0,0 +1,7 @@
+import time
+
+print("12 часовой формат:")
+print(time.strftime("%I:%M"))
+
+print("\nдо/после полуночи:")
+print(time.strftime("%A %p"))
