6626df012fc83b59231b8cee1270f6c9db0caed3
ms33 <ms33@inbox.ru>
2021-01-23 06:37:59 +0300

    2021-01-22_stepik001146ch07p08_nestedLoops

---
+++ 001146StepikPyBegin/Stepik001146PyBeginсh07p08st04CODE03_20210122.py
@@ -0,0 +1,10 @@
+'''
+ Что покажет приведенный ниже фрагмент кода?
+'''
+counter = 0
+for i in range(99, 102):
+    temp = i
+    while temp > 0:
+        counter += 1
+        temp //= 10
+print(counter)
