55bcdec483cf7dc7c31f90caf41c1569ab22c149
ms33 <ms33@inbox.ru>
2021-03-01 08:36:49 +0300

    2021-02-29_Stepik001146functionsch13_var-glob

---
+++ 001146StepikPyBegin/Stepik001146PyBeginсh13p03st09_functions_THEORY02_20210228.py
@@ -0,0 +1,18 @@
+'''
+Глобальные переменные
+'''
+birds = 5000  # глобальная переменная
+
+
+def print_texas():
+    print('В Техасе обитает', birds, 'птиц.')
+
+
+def print_california():
+    print('В Калифорнии обитает', birds, 'птиц.')
+
+
+print_texas()
+print_california()
+
+
