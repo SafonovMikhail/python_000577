a4e972300b1b6acd68ea1cabfb4d5a01ec8e3d01
ms33 <ms33@inbox.ru>
2021-03-07 23:33:39 +0300

    2021-03-07_Step001257PythonBasics_pres07

--- 001257DFedorovPythonBasics/DFedorov001257PythonBasicsPres07p08_tuple_20200502.py
+++ 001257DFedorovPythonBasics/DFedorov001257PythonBasicsPres07p08_tuple_20200502.py
@@ -1,15 +1,24 @@
 t1 = (4,)
 print('t1: ', t1)
-print(len(t1))
+print('len(t1):',len(t1))
 b = ('1', 2, '4')
 print('b: ', b)
 t = tuple(range(11))
-print(t)
+print('t:',t)
 print('t1 + b: ', t1 + b)
 t2 = (1, [1, 2], 3)
-t2[1][0] = 0 # изменение изменяемого типа!
-print(t2)
+print('t2:', t2)
+t2[1][0] = 0  # изменение изменяемого типа!
+print('t2:', t2)
 
+# 20210307
+# для списков:
+print('список:')
 animals = ["cat", "dog", "bird"]
-for index, value in enumerate(animals): # enumerate() функция, которая возвращает индекс и значение для списка
-    print (index,value)
+for index, value in enumerate(animals):  # enumerate() функция, которая возвращает индекс и значение для списка
+    print(index, value)
+# для кортежей:
+print('кортеж:')
+animals = ("cat", "dog", "bird")
+for index, value in enumerate(animals):  # enumerate() функция, которая возвращает индекс и значение для списка
+    print(index, value)
