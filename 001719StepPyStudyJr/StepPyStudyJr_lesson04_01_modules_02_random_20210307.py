abf6b0fbd0fef5c47c0e19e75293d73926a4d04f
ms33 <ms33@inbox.ru>
2021-03-07 23:23:50 +0300

    2021-03-06_StepPyJunior_01-02

---
+++ 001719StepPyStudyJr/StepPyStudyJr_lesson04_01_modules_02_random_20210307.py
@@ -0,0 +1,6 @@
+import random
+
+print(random.random())  # 0.4956579385740163
+print(random.randint(0, 1))  # 0
+print(random.randint(1, 100))  # 66
+print(random.randint(10, 20))  # 16
---
+++ 001719StepPyStudyJr/StepPyStudyJr_lesson04_02_conditional_01_20210306.py
@@ -0,0 +1,5 @@
+time = input("Enter the time: ")
+if int(time) < 12:
+    print("The 1-rst half of the day!")
+else:
+    print("Afternoon!")
